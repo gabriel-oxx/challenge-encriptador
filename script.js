2316e94818aca996c9c0438328c77757c4e0ffb5
Gabriel <gabrielcsilvasg@gmail.com>
2024-02-10 12:15:24 -0300

    Adicionar função de descriptografia

--- script.js
+++ script.js
@@ -2,21 +2,30 @@ const input = document.querySelector("#input");
 const result = document.querySelector("#result");
 const encrypt = document.querySelector("#encrypt");
 const decrypt = document.querySelector("#decrypt");
+const letters = ["a", "e", "i", "o", "u"];
+const correspondence = ["ai", "enter", "imes", "ober", "ufat"];
+let text;
+let newText;
 
 function encryptText() {
-	const letters = ["a", "e", "i", "o", "u"];
-	const correspondence = ["ai", "enter", "imes", "ober", "ufat"];
+  const regex = new RegExp(letters.join("|"), "g");
+  text = input.value;
+  input.value = "";
+  newText = text.replace(regex, (param) => {
+    const index = letters.indexOf(param);
+    return correspondence[index];
+  });
+  result.innerText = newText;
+}
 
-	const text = input.value;
-	let newText = text;
-	const regex = new RegExp(letters.join("|"), "g");
-
-	newText = text.replace(regex, (param) => {
-		const index = letters.indexOf(param);
-		return correspondence[index];
-	});
-	input.value="";
-	console.log(newText);
+function decryptText() {
+  const regex = new RegExp(correspondence.join("|"), "g");
+  newText = newText.replace(regex, (param) => {
+    const index = correspondence.indexOf(param);
+    return letters[index];
+  });
+  result.innerText = newText;
 }
 
 encrypt.addEventListener("click", encryptText);
+decrypt.addEventListener("click", decryptText);
